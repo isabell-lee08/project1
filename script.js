647dec682065c83e4d1b80cabeaa434feca940be
isabell-lee08 <isabellleee8@gmail.com>
2025-09-20 13:06:36 -0400

    scroll box, bg test, layout work

---
+++ script.js
@@ -0,0 +1,36 @@
+// document.addEventListener("DOMContentLoaded", (event) => {
+//     // gsap code here!
+//     gsap.registerPlugin(ScrollTrigger);
+
+//     const thisChorg = document.querySelector('#thischorg')
+//     const chorg = document.querySelector('#chorg')
+
+//     gsap.to(thisChorg, {
+//         scrollTrigger: {
+//             trigger: thisChorg,
+//             scroller: '#main_container',
+//             markers: true,
+//             pin: true,
+//             start: 'top 30%',
+//             scrub: true,
+//             end: 3500,
+//         }
+//     });
+
+//     gsap.to(chorg, {
+//         opacity: 1,
+//         scrollTrigger: {
+//             trigger: chorg,
+//             scroller: '#main_container',
+//             markers: true,
+//             pin: true,
+//             start: 'top 30%',
+//             scrub: true,
+//             end: 3500,
+//         }
+//     });
+
+
+// // Chorg is an alien who needs a new home. Help him find a suitable planet to live.
+
+// });
