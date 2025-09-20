fc7c6ce680e58870224581d9c75409657caaa341
isabell-lee08 <isabellleee8@gmail.com>
2025-09-20 13:56:48 -0400

    new overlay, mobile test

--- script.js
+++ script.js
@@ -1,36 +1,23 @@
-// document.addEventListener("DOMContentLoaded", (event) => {
-//     // gsap code here!
-//     gsap.registerPlugin(ScrollTrigger);
+document.addEventListener("DOMContentLoaded", (event) => {
+    // gsap code here!
+    gsap.registerPlugin(ScrollTrigger);
 
-//     const thisChorg = document.querySelector('#thischorg')
-//     const chorg = document.querySelector('#chorg')
+    const thisChorg = document.querySelector('#thischorg')
+    const chorg = document.querySelector('#chorg')
 
-//     gsap.to(thisChorg, {
-//         scrollTrigger: {
-//             trigger: thisChorg,
-//             scroller: '#main_container',
-//             markers: true,
-//             pin: true,
-//             start: 'top 30%',
-//             scrub: true,
-//             end: 3500,
-//         }
-//     });
-
-//     gsap.to(chorg, {
-//         opacity: 1,
-//         scrollTrigger: {
-//             trigger: chorg,
-//             scroller: '#main_container',
-//             markers: true,
-//             pin: true,
-//             start: 'top 30%',
-//             scrub: true,
-//             end: 3500,
-//         }
-//     });
+    gsap.to(thisChorg, {
+        scrollTrigger: {
+            trigger: thisChorg,
+            scroller: '#main_container',
+            markers: true,
+            pin: true,
+            start: 'top 30%',
+            scrub: true,
+            end: 3500,
+        }
+    });
 
 
-// // Chorg is an alien who needs a new home. Help him find a suitable planet to live.
+// Chorg is an alien who needs a new home. Help him find a suitable planet to live.
 
-// });
+});
